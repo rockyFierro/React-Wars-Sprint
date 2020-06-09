711f56ebc194cb31cdb9e68b95842f9351951ffa
rockyFierro <finnslighthouse@gmail.com>
2020-06-09 15:28:32 -0700

    deleted bloat code

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,11 +8,15 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  /*
+  TODO:
+  -delete bloat code
+  -
+
+  */
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+  null
   );
 }
 
