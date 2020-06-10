95cba0e50e5cd270813a4a193395235b76b28898
rockyFierro <finnslighthouse@gmail.com>
2020-06-09 23:15:32 -0700

    mvp logic is met, but needs a little cleaning up

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
-import PokemonList from "./components/PokemonList";
+import React, { useState, useEffect } from 'react';
+import PokemonList from "./components/Character";
+import axios from 'axios';
+
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,12 +12,12 @@ const App = () => {
   // sync up with, if any.
   /*
   personal naming conventions----------------------------------------------------.
-  |  prefix'd:                                                                   |\
-  |  im = immutable                                                              |\
-  |  mut= mutable                                                                |\
-  '------------------------------------------------------------------------------'\ 
-    \  \     \\\\   \\\\   \\\\             \      \\    \\ \ \ \ \\\\    \ \\  \\\
-  
+    im = immutable                                                              
+    mut= mutable                                                                
+    res = result
+
+
+
   TODO:
   -delete bloat code
   -comps: pokemon list
@@ -23,14 +26,44 @@ const App = () => {
   -gonne use state, import apropriate hooks
   -states: a pokemon list, the next list
   -import'ant to remember: make sure to import other components as they are needed
-  -
-
+  -use axios to get the pokemon
+  -console log results
+  -return results *data.results.map(subject of results array value => name of array value) dont forget to iterrate through the results. CHECK FOR I KNOW IM GOING TO FORGET ERROR MESSAGE
+  -worry about the axios portion first and build outward, once you see the information//effect you want go ahead and wrap it up in an useEffect hook
   */
 
 
-  const [ imPokemon,mutPokemon ] = useState(['pikachu','farfetched']);//notes: returns two variables inside of an array.[immutable/mutable]=useState(defaultState)
+  const [ pokemon,setPokemon ] = useState([]);//notes: returns two variables inside of an array.[immutable/mutable]=useState(defaultState)
+  const [ currentPageURL, setCurrentPageURL ] = useState("https://pokeapi.co/api/v2/pokemon?limit=5");
+  const [ nextPageURL, setNextPageURL ] = useState();
+  const [ prevPageURL, setPrevPageURL ] = useState();
+  const [ loading, setLoading ] = useState(true);
+
+
+
+
+  useEffect(()=>{
+    setLoading(true)
+    let cancel
+  axios
+    .get(currentPageURL,{ cancelToken: new axios.CancelToken( c => cancel = c)})
+    .then((res) => {
+      setLoading(false)
+      setPokemon(res.data.results.map((el, index) => `${el.name} ${el.url}`))
+      console.log(res)
+      setNextPageURL(res.data.next);
+      setPrevPageURL(res.data.previous);
+    }); 
+
+    return () => cancel()
+
+ },[currentPageURL])
+  // axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+  //    mutPokemon(res.data.results.map((el, index) => `${el.name} ${el.url}   `))
+  //    }); 
+if(loading) return "Gathering berries, Loading Pokedex, Organizing Pokeballs..."
   return (
-    <PokemonList pokemon={imPokemon}/>//render ( to the index.html in the root div. )
+    <PokemonList pokemon={pokemon}/>//render ( goes straight to the index.html in the root div. , alike placesng textures over objects. )
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,20 @@
-// Write your Character component here
+import React from 'react'
+
+
+/*
+TODO:
+-pass a destructured variable through as props ({ variable })
+-loop through data with map
+-pass a key on anything iterable....iterating
+
+*/
+export default function PokemonList({ pokemon }) {
+    return (
+        <div>
+            {
+                pokemon.map(el => (
+                    <div key={el}>{el}</div>
+                ))}
+        </div>
+    )
+}
